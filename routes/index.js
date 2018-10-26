fbc5bb1c13474ab468caf8acc4a6f721b1bc849e
Dmitry Kshevsky <rudimo@yandex.ru>
2018-10-26 15:35:36 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -84,44 +84,46 @@ router.post('/explain', async (req, res) => {
     }
 });
 
-// router.put('/index/create', async (req, res) => {
-//     try {
-//         const index = req.body.index;
-//
-//         const data = await client.indices.putSettings({
-//             index,
-//             body: {
-//                 settings: {
-//                     analysis: {
-//                         analyzer: {
-//                             tai_analyzer: {
-//                                 type: "custom",
-//                                 tokenizer: "standard",
-//                                 filter: [
-//                                     "lowercase",
-//                                     "russian_morphology",
-//                                     "english_morphology",
-//                                     "my_stopwords",
-//                                     // "word_list",
-//                                     // "word_list_path"
-//                                 ]
-//                             }
-//                         },
-//                         filter: {
-//                             my_stopwords: {
-//                                 type: "stop",
-//                                 stopwords: "а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,these,they,this,to,was,will,with"
-//                             }
-//                         }
-//                     }
-//                 },
+router.put('/index/create', async (req, res) => {
+    try {
+        const query = req.body.index;
+
+        const data = await client.create(query);
+        // const data = await client.indices.create(query);
+        // "word_list",
+        // "word_list_path"
+        res.send(data);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+// {
+//     "index": "index",
+//     "body": {
+//     "settings": {
+//         "analysis": {
+//             "analyzer": {
+//                 "my_analyzer": {
+//                     "type": "custom",
+//                         "tokenizer": "standard",
+//                         "filter": [
+//                         "lowercase",
+//                         "russian_morphology",
+//                         "english_morphology",
+//                         "my_stopwords"
+//                     ]
+//                 }
+//             },
+//             "filter": {
+//                 "my_stopwords": {
+//                     "type": "stop",
+//                         "stopwords": "а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,these,they,this,to,was,will,with"
+//                 }
 //             }
-//         });
-//         res.send(data);
-//     } catch (e) {
-//         res.status(500).send(e);
+//         }
 //     }
-// });
+// }
 
 // router.put('/put-mapping', async (req, res) => {
 //     try {
